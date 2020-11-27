3d07dd968dda9d48a14af539d662fd995d881272
Alexander <54165832+AlexaderLex@users.noreply.github.com>
2020-11-27 14:36:44 +0300

    Add files via upload

--- bootcamp/script.js
+++ bootcamp/script.js
@@ -1,6 +1,32 @@
  "use strict";
 
  const content = document.querySelectorAll(".content");
+ const slider = document.querySelector(".slider");
+ const prev = document.querySelectorAll(".prev");
+ const next = document.querySelectorAll(".next");
 
- const prev = document.getElementById("prev");
- const next = document.getElementById("next");
+ prev.forEach(el => {
+     console.log(el.parentElement.id);
+     el.addEventListener("click", e => {
+         e.preventDefault();
+
+         let contentId = el.parentElement.parentElement.parentElement.id;
+         if (contentId = "tanya") {
+             slider.style.transform = "translateX(0)";
+         } else {
+             slider.style.transform = "translateX(-100%)";
+         }
+     });
+ });
+
+ next.forEach(el => {
+     el.addEventListener("click", e => {
+         e.preventDefault();
+         let contentId = el.parentElement.parentElement.parentElement.id;
+         if (contentId = "tanya") {
+             slider.style.transform = "translateX(-100%)";
+         } else {
+             slider.style.transform = "translateX(0)";
+         }
+     });
+ });
